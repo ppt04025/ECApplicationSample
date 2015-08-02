9cfb8c9dae0dc4dbaa24a5fbcc887e26095159f6
xenonchau <xenonchau@gmail.com>
2015-08-02 20:16:45 +0800

    Initial Commit

---
+++ ECApplicationSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ECApplicationSample
+//
+//  Created by Xenon Chau on 15/8/2.
+//  Copyright (c) 2015年 EasyCoding & Play4Fun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ECApplicationSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ECApplicationSample
+//
+//  Created by Xenon Chau on 15/8/2.
+//  Copyright (c) 2015年 EasyCoding & Play4Fun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
